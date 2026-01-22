5c9a1c654cea7c92ca4ce2a99759dda3ee3a8294
joshuamokayait-blip <joshuamokayait@gmail.com>
2026-01-22 05:14:36 -0800

    Update footer text to include Quantum Inka Technologies

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -172,7 +172,7 @@
             
       
     <div class="footer d-sm-flex align-items-center justify-content-between border-top bg-white p-3">
-        <p class="mb-0 text-gray-9">&copy;  All Right Reserved</p>
+        <p class="mb-0 text-gray-9">&copy;  All Right Reserved Powered with Quantum Inka Technologies</p>
         <!-- <p>Designed &amp; Developed by <a href="javascript:void(0);" class="text-primary"></a></p> -->
     </div>
 </div>
